4f190808bfdb3b1e8f78849a17025e262ccec919
LFM Alves <luis.alves@locaweb.com.br>
2020-03-02 20:41:53 -0300

    feat: post_expense monadic method

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -9,6 +9,15 @@ module ExpenseTracker
     def app
       ExpenseTracker::API.new
     end
+
+    def post_expense(expense)
+      post '/expenses', JSON.generate(coffee)
+      expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => paarsed['expense_id'])
+    end
     
     it 'records submitted expenses' do
       coffee = {
@@ -16,12 +25,6 @@ module ExpenseTracker
         'amount' => 5.75,
         'date' => '2017-06-10'
       }
-
-      post '/expenses', JSON.generate(coffee)
-      expect(last_response.status).to eq(200)
-
-      parsed = JSON.parse(last_response.body)
-      expect(parsed).to include('expense_id' => a_kind_of(Integer))
     end
   end
 end
