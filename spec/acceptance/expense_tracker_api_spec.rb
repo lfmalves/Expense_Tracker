e7f53afafc62aeb69f374f13ddcb7c7a9cc2e3b6
LFM Alves <luis.alves@locaweb.com.br>
2020-03-02 19:51:52 -0300

    feat: initial implementation of api

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -1,10 +1,15 @@
 require 'rack/test'
 require 'json'
+require_relative '../../app/api'
 
 module ExpenseTracker
   RSpec.describe 'Expense Tracker API' do
     include Rack::Test::Methods
 
+    def app
+      ExpenseTracker::API.new
+    end
+    
     it 'records submitted expenses' do
       coffee = {
         'payee' => 'Starbucks',
@@ -13,6 +18,7 @@ module ExpenseTracker
       }
 
       post '/expenses', JSON.generate(coffee)
+      expect(last_response.status).to eq(200)
     end
   end
 end
