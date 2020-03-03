f5411bc76f580c1b12fe9b1b82282b2ab9c01902
LFM Alves <luis.alves@locaweb.com.br>
2020-03-02 21:01:38 -0300

    spec: checking http response for specific day

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -37,6 +37,12 @@ module ExpenseTracker
         'amount' => 95.20,
         'date' => '2017-06-11'
       )
+
+      get'/expenses/2017-06-10'
+      expect(last_response.status).to eq(200)
+
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
     end
   end
 end
