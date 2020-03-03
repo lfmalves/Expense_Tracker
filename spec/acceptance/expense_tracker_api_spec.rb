815dd0b7ccdd885bb0d52f7536683bea941d72da
LFM Alves <luis.alves@locaweb.com.br>
2020-03-02 21:04:07 -0300

    typo

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -11,12 +11,12 @@ module ExpenseTracker
     end
 
     def post_expense(expense)
-      post '/expenses', JSON.generate(coffee)
+      post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
 
       parsed = JSON.parse(last_response.body)
       expect(parsed).to include('expense_id' => a_kind_of(Integer))
-      expense.merge('id' => paarsed['expense_id'])
+      expense.merge('id' => parsed['expense_id'])
     end
     
     it 'records submitted expenses' do
